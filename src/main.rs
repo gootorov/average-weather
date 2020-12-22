9667085e4d40ab7276a8b083b285adf30e007c43
Igor Gutorov <igootorov@gmail.com>
2020-12-22 18:13:17 +0200

    Add index page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+const WELCOME_PAGE: &'static str = "Hello, World\n";
+
+#[get("/")]
+fn index() -> &'static str {
+    WELCOME_PAGE
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
